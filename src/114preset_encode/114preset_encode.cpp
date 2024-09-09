e57b8e765d13776fc58b93f25ee80726a8050413
Natsu903 <1720816774@qq.com>
2024-09-09 09:13:57 +0800

    114 final

--- src/114preset_encode/114preset_encode.cpp
+++ src/114preset_encode/114preset_encode.cpp
@@ -61,18 +61,57 @@ int main(int argc, char *argv[])
 	c->pix_fmt = AV_PIX_FMT_YUV420P; // 元数据格式，与编码算法相关
 	c->thread_count = 16;			 // 编码线程数，可以调用系统借口获取cpu核心数量
 
-	//预设编码器参数
-	c->max_b_frames = 0;
-	int opt_re = av_opt_set(c->priv_data, "preset", "ultrafast", 0);//最快速度
-	if (opt_re != 0)
-	{
-		cout << "preset failed" << endl;
-	}
-	opt_re = av_opt_set(c->priv_data, "tune", "zerolatency", 0);//零延迟
-	if (opt_re != 0)
-	{
-		cout << "preset failed" << endl;
-	}
+	/**
+	 * .
+	 * ABR 平均比特率
+	 */
+	int br = 400000;
+	//c->bit_rate = br;
+
+	/**
+	 * .
+	 * CQP 恒定质量
+	 * H.264的QP范围从0到51
+	 * x264默认23 效果较好18
+	 * x265默认28 效果较好25
+	 */
+	//av_opt_set_int(c->priv_data, "qp", 0,0);
+
+	/**
+	 * .
+	 * CBR 恒定比特率 
+	 */
+	//c->rc_min_rate = br;
+	//c->rc_max_rate = br;
+	//c->rc_buffer_size = br;
+	//c->bit_rate = br;
+	//av_opt_set(c->priv_data, "nal-hrd", "cbr", 0);
+
+	/**
+	 * .
+	 * 恒定速率因子(CRF)
+	 */
+	av_opt_set_int(c->priv_data, "crf", 23, 0);
+
+	/**
+	 * .
+	 * 约束编码(VBV)
+	 */
+	c->rc_max_rate = br;
+	c->rc_buffer_size = br * 2;
+
+	////预设编码器参数
+	//c->max_b_frames = 0;
+	//int opt_re = av_opt_set(c->priv_data, "preset", "ultrafast", 0);//最快速度
+	//if (opt_re != 0)
+	//{
+	//	cout << "preset failed" << endl;
+	//}
+	//opt_re = av_opt_set(c->priv_data, "tune", "zerolatency", 0);//零延迟 h265不支持b_frame
+	//if (opt_re != 0)
+	//{
+	//	cout << "preset failed" << endl;
+	//}
 
 	// 4.打开编码上下文
 
