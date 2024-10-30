cf82d30ef7ee969da06d7580a7533bef481c7b73
natsu903 <1720816774@qq.com>
2024-10-30 10:46:32 +0800

    完成视频重编码为h265并写重新封装MP4

---
+++ src/125_record_remux/XDemux.cpp
@@ -0,0 +1,52 @@
+﻿#include "XDemux.h"
+#include <iostream>
+extern "C"
+{
+#include <libavformat/avformat.h>
+}
+
+#pragma comment(lib,"avformat.lib")
+#pragma comment(lib,"avutil.lib")
+#pragma comment(lib,"avcodec.lib")
+using namespace std;
+
+static void PrintErr(int err)
+{
+	char buf[1024] = { 0 };
+	av_strerror(err, buf, sizeof(buf) - 1);
+	cerr << buf << endl;
+}
+
+#define BERR(err) if(err!=0){PrintErr;return 0;}
+
+AVFormatContext* XDemux::Open(const char* url)
+{
+    AVFormatContext* c = nullptr;
+    auto re = avformat_open_input(&c, url, nullptr, nullptr);
+    BERR(re);
+    //获取媒体信息
+    re = avformat_find_stream_info(c, nullptr);
+    BERR(re);
+    //打印输入封装信息
+    av_dump_format(c, 0, url, 0);
+
+    return c;
+}
+
+bool XDemux::Read(AVPacket* pkt)
+{
+    unique_lock<mutex> lock(mux_);
+    if (!c_) return false;
+    auto re = av_read_frame(c_, pkt);
+    BERR(re);
+    return true;
+}
+
+bool XDemux::Seek(long long pts, int stream_index)
+{
+	unique_lock<mutex> lock(mux_);
+	if (!c_) return false;
+	auto re = av_seek_frame(c_, stream_index, pts, AVSEEK_FLAG_FRAME | AVSEEK_FLAG_BACKWARD);//向后关键帧
+	BERR(re);
+	return true;
+}
---
+++ src/125_record_remux/XFormat.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+/*****************************************************************//**
+ * \file   XFormat.h
+ * \brief  封装和解封装的基类
+ * 
+ * \author Administrator
+ * \date   October 2024
+ *********************************************************************/
+#include <mutex>
+
+struct AVFormatContext;
+struct AVCodecParameters;
+struct AVPacket;
+struct AVCodecContext;
+struct XRational
+{
+	int num;
+	int den;
+};
+class XFormat
+{
+public:
+	/**
+	 * 复制参数 线程安全.
+	 * 
+	 * \param stream_index 对应c_->streams下标
+	 * \param dst 输出参数
+	 * \return 是否成功
+	 */
+	bool CopyPara(int stream_index,AVCodecParameters* dst);
+	bool CopyPara(int stream_index, AVCodecContext* dst);
+	/**
+	 * 设置上下文，并且清理上次设置的值，如果传递NULL,相当于关闭上下文.
+	 * 
+	 * \param AVFormatContext* c
+	 */
+	void set_c(AVFormatContext* c);
+	int audio_index() { return audio_index_; }
+	int video_index() { return video_index_; }
+	XRational video_time_base() { return video_time_base_; }
+	XRational audio_time_base() { return audio_time_base_; }
+	//根据timebase换算时间
+	bool RescaleTime(AVPacket* pkt,long long offset_pts, XRational time_base);
+	int video_codec_id() { return video_codec_id_; }
+
+protected:
+	AVFormatContext* c_=nullptr;//封装解封装上下文
+	std::mutex mux_;//资源互斥
+	int video_index_ = 0;//video在stream中的索引
+	int audio_index_ = 1;//audio在stream中的索引
+	XRational video_time_base_ = { 1,25 };
+	XRational audio_time_base_ = { 1,9000 };
+	int video_codec_id_ = 0;//编码器id
+};
+
