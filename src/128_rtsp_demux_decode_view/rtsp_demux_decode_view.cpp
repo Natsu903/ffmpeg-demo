01900497bbc96d7f9015387f48b8a29b3e240458
natsu903 <1720816774@qq.com>
2024-11-04 16:56:01 +0800

    完成rtsp的多线程责任链解封装解码和渲染

---
+++ src/128_rtsp_demux_decode_view/rtsp_demux_decode_view.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <thread>
+#include "xtools.h"
+#include "xdemux_task.h"
+#include "xdecodetask.h"
+#include "xvideo_view.h"
+
+using namespace std;
+
+//class TestThread :public XThread
+//{
+//	void Main() override
+//	{
+//		LOGDEBUG("TestThread Main() begin");
+//		while (!is_exit_)
+//		{
+//			this_thread::sleep_for(1ms);
+//		}
+//		LOGDEBUG("TestThread Main() end");
+//	}
+//};
+
+#define RTSP_URL "rtsp://127.0.0.1:8554/test"  //测试流
+//#define RTSP_URL "rtsp://admin:GZH&password@192.168.31.234:554/Streaming/Channels/101"	//监控流
+
+int main(int argc, char* argv[])
+{
+	//TestThread tt;
+	//tt.Start();
+	//this_thread::sleep_for(3s);
+	//tt.Stop();
+	XDemuxTask demux_task;
+	while (true)
+	{
+		if (demux_task.Open(RTSP_URL))
+		{
+			break;
+		}
+		MSleep(100);
+		continue;
+	}
+	auto para = demux_task.CopyVideoPara();
+	//初始化渲染
+	auto view = XVideoView::Create();
+	view->Init(para->para);
+
+	XDecodeTask decode_task;
+	if (!decode_task.Open(para->para))
+	{
+		LOGERROR("open decode failed");
+	}
+	else
+	{
+		demux_task.set_next(&decode_task);
+		demux_task.Start();
+		decode_task.Start();
+	}
+
+	while (true)
+	{
+		auto f = decode_task.GetFrame();
+		if (!f)
+		{
+			MSleep(1);
+			continue;
+		}
+		view->DrawFrame(f);
+		XFreeFrame(&f);
+	}
+
+	getchar();
+	//system("pause");
+	return 0;
+}
---
+++ src/128_rtsp_demux_decode_view/xdecodetask.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "xtools.h"
+#include "xdecode.h"
+
+class XDecodeTask:public XThread
+{
+public:
+	/**
+	 * 打开解码器.
+	 * 
+	 * \param para
+	 * \return 
+	 */
+	bool Open(AVCodecParameters* para);
+	//责任链处理函数
+	void Do(AVPacket* pkt) override;
+	//线程主函数
+	void Main() override;
+
+	//返回当前需要渲染的AVFrame，如果没有返回null，线程安全
+	//需要need_view_控制渲染,返回结果需要用XFreeFrame释放
+	AVFrame* GetFrame();
+private:
+	std::mutex mux_;
+	XDecode decode_;
+	XAVPacketList pkt_list_;
+	AVFrame* frame_ = nullptr;//解码后存储
+	bool need_view_ = false;//是否需要渲染，每帧只渲染一次
+};
+
